7b331d849324835a201326cccf13cbdc34744ddd
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2025-11-22 15:40:26 +0900

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <main className="flex flex-col items-center m-10">
+      <h1 className="text-3xl">
+        Getting Started.
+      </h1>
+      <div>
+        <img src={'./img/puyo_00_jama.png'} />
+        <img src={'./img/puyo_01_red.png'} />
+        <img src={'./img/puyo_02_green.png'} />
+        <img src={'./img/puyo_03_blue.png'} />
+        <img src={'./img/puyo_04_yellow.png'} />
+        <img src={'./img/puyo_05_purple.png'} />
+      </div>
+    </main>
+  )
+}
---
+++ src/components/animrow.jsx
@@ -0,0 +1,6 @@
+export default function AnimRow() {
+  return (
+    <section>
+    </section>
+  )
+}
